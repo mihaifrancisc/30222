eb9da613f5c9e62e405f4a3b516a68304c7dd014
AlinaChibici <alina.chibici@gmail.com>
2016-10-15 14:15:35 +0300

    Update Even.java

--- Students/Chibici I. Alina Roxana/fibonacci/src/fibonacci/Even.java
+++ Students/Chibici I. Alina Roxana/fibonacci/src/fibonacci/Even.java
@@ -1,13 +1,13 @@
 package fibonacci;
 
-public class Even{
-	//metoda statica ca sa vada daca un numar e par
-	//sa o aplicam direct pe clasa sa nu avem nevoie de obiect
+public class Even {
+	//static method to see if a number is even
+	//we will apply it directly on the class - no object needed
 	 public static Boolean isEven (int n) {
 		 if(n % 2 == 0){
 		 	return true;
 		 }
-		 else{
+		 else {
 		 	return false;
 		 }
 	}
