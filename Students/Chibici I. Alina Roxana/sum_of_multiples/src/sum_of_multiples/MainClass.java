857352331b14b483497352970c619846f7b574ec
AlinaChibici <alina.chibici@gmail.com>
2016-10-15 14:13:24 +0300

    Update MainClass.java

--- Students/Chibici I. Alina Roxana/sum_of_multiples/src/sum_of_multiples/MainClass.java
+++ Students/Chibici I. Alina Roxana/sum_of_multiples/src/sum_of_multiples/MainClass.java
@@ -1,17 +1,17 @@
 package sum_of_multiples;
-//apelam clasa pentru a putea lua numarul de la tastatura
+//class for reading a number from keyboard
 import java.util.Scanner;
 
-//suma multiplilor lui 3 si 5 pana la 1000
+//the sum of the multiples of 3 and 5 under 1000
 public class MainClass {
 
 	public static void main(String[] args) {
-		//cream 2 numere pe care sa aplicam metoda de calculare a sumei multiplilor
+		//create 2 numbers on which to apply the method of cumputing the sum of multiples
 		MultipleOfX firstNumber = new MultipleOfX(3);
 		MultipleOfX secondNumber = new MultipleOfX(5);
-		//cream obiectul tastatura pe care sa apelam metoda de citire a urmatoarului simbol 
+		//create the object keyboard on which we apply the method of reading from keyboard
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Multiplii pana la: ");
+		System.out.println("Multiples up to: ");
 		long n = keyboard.nextLong();
 		long sum = firstNumber.sumOfMultiples(n) + secondNumber.sumOfMultiples(n);
 		System.out.println(sum);
@@ -19,7 +19,7 @@ public class MainClass {
 	}
 
 }
-//ultimul numar de tipul int pentru n ce da un rezultat corect e 89739 - rezultat: 2 147 418 374
+//the last number of type int which shows a correct result is 89739 - result: 2 147 418 374
 // the maximum of int is 2 147 483 647 
 //  the maximum of long is 9,223,372,036,854,775,807
 // it takes way too much time to find out the last value that gives the right answer
