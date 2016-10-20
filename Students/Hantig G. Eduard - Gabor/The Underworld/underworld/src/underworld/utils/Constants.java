d77694ee0c4e35601c1d8061fb1f45377791570b
EduardHantig <Eduard@Eduards-MacBook-Pro.local>
2016-10-21 02:11:58 +0300

    TheUnderworld
    Sorry for this unexpected “Delay”..

---
+++ Students/Hantig G. Eduard - Gabor/The Underworld/underworld/src/underworld/utils/Constants.java
@@ -0,0 +1,10 @@
+package underworld.utils;
+
+public class Constants {
+	public final static String WORLD_NAME = "Terra";
+	public final static String PATH_TO_RULES_FILE = "//Users//Eduard//Documents//workspace//30222//Students//Hantig G. Eduard-Gabor//The Underworld";
+	public final static int MINER_BACKPACK_SIZE = 5;
+	public final static int MAP_RESOURCES_NUMBER = 10;
+	public final static int NUMBER_OF_ROUNDS = 5;
+
+}
