65f656be27ffec2b115fc17dc76300d74ef143aa
AlinaChibici <alina.chibici@gmail.com>
2016-10-20 22:05:59 +0300

    I had a lot of fun doing this homework. Please make more similar assignments.

---
+++ Students/Chibici I. Alina Roxana/Underworld/src/underworld/utils/Constants.java
@@ -0,0 +1,9 @@
+package underworld.utils;
+
+public class Constants {
+	public static final String WORLD_NAME = "Terabithia";
+	public static final String PATH_TO_RULES_FILE = "C:/Users/Alina/Desktop/30222/Resources/The Underworld/rules.txt";
+	public static final int MINER_BACKPACK_SIZE = 5;
+	public static final int MAP_RESOURCES_NUMBER = 10;
+	public static final int NUMBER_OF_ROUNDS = 5; // NOT a String as in the documentation			
+}
