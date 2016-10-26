1d5e517f38c8bf34f78feb6138e125033d01f511
cosminadr09 <cosminadr09@gmail.com>
2016-10-26 23:59:34 +0300

    Am completat toate functiile dar nu sunt sigura pe clasa FileReader ce am realizat.

--- Students/Dragota M. Cosmina - Maria/underworld/src/underworld/utils/Constants.java
+++ Students/Dragota M. Cosmina - Maria/underworld/src/underworld/utils/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static String PATH_TO_RULES_FILE = "D:/AN 2/OOP/30222/Students/Dragota M. Cosmina - Maria";
 	public static int MINER_BACKPACK_SIZE = 5;
 	public static int MAP_RESOURCES_NUMBER = 10;
-	public static int NUMBER_OF_ROUNDS = 5;// in specificatie apare ca string
+	public static int NUMBER_OF_ROUNDS = 5;
 
 }
--- Students/Dragota M. Cosmina - Maria/underworld/src/underworld/utils/Resources.java
+++ Students/Dragota M. Cosmina - Maria/underworld/src/underworld/utils/Resources.java
@@ -5,12 +5,43 @@ import java.util.Random;
 public class Resources {
 	private static Random random = new Random();
 
-	public static int generateValuableResource() {
-		return 0;
+	/**
+	 * Generate a random number between 0 and Constants.MAP_RESOURCES_NUMBER;
+	 * limits included.
+	 * 
+	 * @return
+	 */
+	public static int generateValuableResource() { // o metoda statica se
+													// apeleaza folosind numele
+													// clasei
+
+		int i;
+		int[] randomValue;
+		randomValue = new int[100];
+		for (i = 0; i < Constants.MAP_RESOURCES_NUMBER; i++) {
+			randomValue[i] = (int) (Math.random() * 100);// return a value
+															// between 0-99
+		}
+
+		return randomValue[i];
 	}
 
+	/**
+	 * Generate a random number between 0 and Constants.MAP_RESOURCES_NUMBER * 2;
+	 * limits included.
+	 * 
+	 * @return
+	 */
 	public static int generateResource() {
-		return 0;
+		int i;
+		int[] randomValue;
+		randomValue = new int[100];
+		for (i = 0; i < (Constants.MAP_RESOURCES_NUMBER * 2); i++) {
+			randomValue[i] = (int) (Math.random() * 200);// return a value
+															// between 0-99
+		}
+
+		return randomValue[i];
 	}
 
 }
