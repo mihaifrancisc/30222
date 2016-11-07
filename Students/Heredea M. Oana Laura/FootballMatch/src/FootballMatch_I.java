dbd1ae69d0871233d685be069d0698d149458029
Oana Heredea <oanaheredea@yahoo.com>
2016-11-07 11:18:23 +0200

    Update FootballMatch_I.java

--- Students/Heredea M. Oana Laura/FootballMatch/src/FootballMatch_I.java
+++ Students/Heredea M. Oana Laura/FootballMatch/src/FootballMatch_I.java
@@ -5,7 +5,9 @@ public interface FootballMatch_I extends SportsMatch_I{
 	public static final int FULL_TIME = 40;
 	
 	public void homeTeamScored();
+	
 	public void awayTeamScored();
+	
 	public String getMatchScore();
 	
 
