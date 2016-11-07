f80a6ffe25f127434c72723ea0725fa494af4249
Oana Heredea <oanaheredea@yahoo.com>
2016-11-07 11:16:10 +0200

    Update SportsMatch_I.java

--- Students/Heredea M. Oana Laura/FootballMatch/src/SportsMatch_I.java
+++ Students/Heredea M. Oana Laura/FootballMatch/src/SportsMatch_I.java
@@ -2,7 +2,10 @@
 public interface SportsMatch_I {
 	
 public String getHomeTeamName();
+	
 public void setHomeTeamName(String name);
+	
 public String getAwayTeamName();
+	
 public void setAwayTeamName(String name);
 }
