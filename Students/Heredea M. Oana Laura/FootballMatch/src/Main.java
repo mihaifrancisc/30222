505d61d1dc436626631d2a1f010a6e37c4058ee8
Heredea M. Oana Laura <oanaheredea@yahoo.com>
2016-11-07 10:54:22 +0200

    Football Match Lab 5 - finished

---
+++ Students/Heredea M. Oana Laura/FootballMatch/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s;
+		// TODO Auto-generated method stub
+		FootballMatch fm = new FootballMatch("BM" , "CJ");
+		Random r1 = new Random();
+		for(int i = 0; i < fm.FULL_TIME; i++){
+			int tmp = r1.nextInt(2);
+			//System.out.print(tmp);
+			switch(tmp){
+			case 0 : fm.homeTeamScored();break;
+			case 1 : fm.awayTeamScored();break;
+			default : System.out.println("invalid number");
+			
+			}
+		}s = fm.getMatchScore();
+		System.out.println("scorul:" + s );
+	}
+
+}
---
+++ Students/Heredea M. Oana Laura/FootballMatch/src/SportsMatch_I.java
@@ -0,0 +1,8 @@
+
+public interface SportsMatch_I {
+	
+public String getHomeTeamName();
+public void setHomeTeamName(String name);
+public String getAwayTeamName();
+public void setAwayTeamName(String name);
+}
